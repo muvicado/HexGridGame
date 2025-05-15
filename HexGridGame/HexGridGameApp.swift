dd6a3ba6c614b15ef6e66952a5d81e533d80a7e0
Mark Barclay <markbarclay@mac.com>
2025-05-15 14:37:40 -0400

    Initial Commit

---
+++ HexGridGame/HexGridGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexGridGameApp.swift
+//  HexGridGame
+//
+//  Created by Mark Barclay on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HexGridGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
